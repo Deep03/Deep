0cf0614e9cb1df5bdcf80378238a81cb7c384d4d
Deep03 <59480224+Deep03@users.noreply.github.com>
2021-12-01 10:08:49 -0500

    Add files via upload

---
+++ Renderer.java
@@ -0,0 +1,19 @@
+
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+
+        Main.flappyBird.paint(g);
+    }
+
+}
